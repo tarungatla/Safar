dae0ed78ca115c97ab0562f94bceecbfabde29f8
tarungatla <tarungatla75@gmail.com>
2024-02-09 21:40:24 +0530

    Slider in Discover

--- js/script.js
+++ js/script.js
@@ -47,7 +47,7 @@ var swiper = new Swiper(".review-slider", {
     spaceBetween: 20,
     loop: true,
     autoplay: {
-        delay: 1500,
+        delay: 2500,
         disableOnInteraction: false,
     },
     breakpoints: {
